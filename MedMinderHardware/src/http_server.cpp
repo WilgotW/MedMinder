87c3b27a2f959c5424ad4483ab524eb14bf00f96
WilgotW <wilgotwardell@hotmail.com>
2025-04-04 15:40:33 +0200

    -

--- MedMinderHardware/src/http_server.cpp
+++ MedMinderHardware/src/http_server.cpp
@@ -8,6 +8,5 @@ void setupServer() {
   server.on("/ping", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send(200, "text/plain", "pong from http_server.cpp");
   });
-
   server.begin();
 }
