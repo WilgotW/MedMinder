b4afcf2525cff84f6ee73b8eef575e02fde35d40
WilgotW <wilgotwardell@hotmail.com>
2025-04-08 21:15:34 +0200

    -

--- MedMinderAPI/src/scheduler/doseCheck.ts
+++ MedMinderAPI/src/scheduler/doseCheck.ts
@@ -3,62 +3,57 @@ import prisma from "../lib/prisma";
 import { differenceInMinutes } from "date-fns";
 import { sendPushNotification } from "../utils/sendPushNotifications";
 
+cron.schedule("* * * * *", () => {
+  console.log("Running task every minute at", new Date().toLocaleTimeString());
+});
+
 export async function runDoseCheck() {
-  const now = new Date();
-
-  const doses = await prisma.dose.findMany({
-    where: {
-      dispensed: false,
-      // This condition does not filter anything if userId is always non-null:
-      userId: { not: 0 },
-    },
-  });
-
-  for (const dose of doses) {
-    const [hours, minutes] = dose.time.split(":").map(Number);
-    const targetTime = new Date(
-      now.getFullYear(),
-      now.getMonth(),
-      now.getDate(),
-      hours,
-      minutes
-    );
-
-    const diff = Math.abs(differenceInMinutes(now, targetTime));
-    if (diff <= 2) {
-      console.log(`Time to dispense: ${dose.medicine} at ${dose.time}`);
-
-      await prisma.dose.update({
-        where: { id: dose.id },
-        data: { dispensed: true },
-      });
-
-      if (!dose.userId) {
-        console.warn(`Dose ${dose.id} is missing userId. Skipping.`);
-        continue;
-      }
-
-      const user = await prisma.user.findUnique({
-        where: { id: dose.userId },
-      });
-
-      console.log("Checking dose:", dose);
-
-      if (user?.expoToken) {
-        try {
-          await sendPushNotification(
-            user.expoToken,
-            `💊 Time to dispense ${dose.medicine} at ${dose.time}`
-          );
-          console.log(`Push notification sent to user ${dose.userId}`);
-        } catch (error) {
-          console.error("Error sending push notification:", error);
-        }
-      } else {
-        console.warn(`No expo token found for user ${dose.userId}`);
-      }
-
-      // TODO: SEND COMMAND TO ESP
-    }
-  }
+  // const now = new Date();
+  // const doses = await prisma.dose.findMany({
+  //   where: {
+  //     dispensed: false,
+  //     // This condition does not filter anything if userId is always non-null:
+  //     userId: { not: 0 },
+  //   },
+  // });
+  // for (const dose of doses) {
+  //   const [hours, minutes] = dose.time.split(":").map(Number);
+  //   const targetTime = new Date(
+  //     now.getFullYear(),
+  //     now.getMonth(),
+  //     now.getDate(),
+  //     hours,
+  //     minutes
+  //   );
+  //   const diff = Math.abs(differenceInMinutes(now, targetTime));
+  //   if (diff <= 2) {
+  //     console.log(`Time to dispense: ${dose.medicine} at ${dose.time}`);
+  //     await prisma.dose.update({
+  //       where: { id: dose.id },
+  //       data: { dispensed: true },
+  //     });
+  //     if (!dose.userId) {
+  //       console.warn(`Dose ${dose.id} is missing userId. Skipping.`);
+  //       continue;
+  //     }
+  //     const user = await prisma.user.findUnique({
+  //       where: { id: dose.userId },
+  //     });
+  //     console.log("Checking dose:", dose);
+  //     if (user?.expoToken) {
+  //       try {
+  //         await sendPushNotification(
+  //           user.expoToken,
+  //           `💊 Time to dispense ${dose.medicine} at ${dose.time}`
+  //         );
+  //         console.log(`Push notification sent to user ${dose.userId}`);
+  //       } catch (error) {
+  //         console.error("Error sending push notification:", error);
+  //       }
+  //     } else {
+  //       console.warn(`No expo token found for user ${dose.userId}`);
+  //     }
+  //     // TODO: SEND COMMAND TO ESP
+  //   }
+  // }
 }
