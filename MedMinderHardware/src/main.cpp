b47f64fd6ae76c1a11ac6bd789111dacdf4fb38a
WilgotW <wilgotwardell@hotmail.com>
2025-04-15 18:06:46 +0200

    -

--- MedMinderHardware/src/main.cpp
+++ MedMinderHardware/src/main.cpp
@@ -1,28 +1,53 @@
+// #include <Arduino.h>
+// #include "globals.h"
+// #include "wheelLogic.h"
+// #include "server.h"
+
+// void setup() {
+//   Serial.begin(115200);
+//   Serial.println("Hello!");
+
+//   serverSetup(); 
+
+//   servo.attach(13, minPulse, maxPulse);
+//   servo.writeMicroseconds(minPulse);
+//   delay(10000);
+// }
+
+// unsigned long previousMillis = 0;
+// const unsigned long interval = 10000; 
+
+// void loop() {
+//   unsigned long currentMillis = millis();
+//   if (currentMillis - previousMillis >= interval) {
+//     previousMillis = currentMillis;
+//     getDose();  
+    
+//   }
+
+// }
+
+
 #include <Arduino.h>
-#include "globals.h"
-#include "wheelLogic.h"
-#include "server.h"
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(23, 22, 21, 18, 17, 16);
+int audioSource = 25;
+
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println("Hello!");
-
-  serverSetup(); 
-
-  servo.attach(13, minPulse, maxPulse);
-  servo.writeMicroseconds(minPulse);
-  delay(10000);
+  lcd.setCursor(8, 0);
+  delay(100);
+  lcd.setCursor(0, 0);
+  lcd.print("Ta medicin");
+  pinMode(audioSource, OUTPUT);
 }
-
-unsigned long previousMillis = 0;
-const unsigned long interval = 10000; 
 
 void loop() {
-  unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;
-    getDose();  
-    
-  }
-
+  // delay(1000);
+  // digitalWrite(audioSource, HIGH);
+  // delay(2000);
+  // digitalWrite(audioSource, LOW);
+  // delay(2000);
 }
+
